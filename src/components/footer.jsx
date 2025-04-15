b7f33aa6667dcd47201981a42d9f373cafd8d261
darskp <darshanpatgar100@gmail.com>
2025-04-16 00:07:43 +0530

    package update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container-fluid">
                 <div className="row bg-dark text-center py-3">
                     <div className="col-lg-12 col-sm-12">
-                        <p className="text-white">Copyright &copy; 2023 | <a href="/home" className="link-primary text-decoration-none">Foodies.com</a></p>
+                        <p className="text-white">Copyright &copy; 2025 | <a href="/home" className="link-primary text-decoration-none">Foodies.com</a></p>
                     </div>
                 </div>
             </div>
